b2868d3ad00eaadbcbb0338b1bec5d15c1088b4b
RahmanSahriar <72905627+RahmanSahriar@users.noreply.github.com>
2020-11-12 16:45:42 +0600

    Lab Task 4

---
+++ lab4/Homepage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Homepage</title>
+  </head>
+  <body style="background-color:powderblue;">
+    <header>
+        <h1 style="display: inline;">Sahriar IT ltd.</h1>
+
+        <nav style="display:inline">
+          <a href="/Lab Task 4/Homepage.php">Home</a> |
+          <a href="/Lab Task 4/Login.php">Login</a> |
+          <a href="/Lab Task 4/Ragistration.php">Registration</a> |
+        </nav>
+    </header>
+    <footer>
+      <span>Copyright &#169 2017</span>
+    </footer>
+
+
+  </body>
+</html>
